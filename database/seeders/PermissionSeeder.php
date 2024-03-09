a56ed9fa015466eab32d1b34a6f7fd80ae1ed035
ryanf27 <ryanfebry0227@gmail.com>
2024-03-09 12:18:10 +0700

    feature: add role and permission seeder

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Permission::create(['name' => 'manage users']);
+        Permission::create(['name' => 'manage classes']);
+        Permission::create(['name' => 'manage subjects']);
+        Permission::create(['name' => 'manage assignments']);
+        Permission::create(['name' => 'manage grades']);
+
+        Permission::create(['name' => 'view grades']);
+        Permission::create(['name' => 'view profile']);
+        Permission::create(['name' => 'view schedule']);
+
+        Permission::create(['name' => 'edit profile']);
+        Permission::create(['name' => 'download materials']);
+    }
+}
---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+
+
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminRole = Role::whereName('admin')->first();
+        $teacherRole = Role::whereName('teacher')->first();
+        $studentRole = Role::whereName('student')->first();
+        $parentRole = Role::whereName('parent')->first();
+
+        $adminRole->givePermissionTo(Permission::all());
+
+        $teacherRole->givePermissionTo([
+            'manage assignments',
+            'manage classes',
+            'manage subjects',
+            'view grades',
+            'view profile',
+            'edit profile',
+            'view schedule',
+        ]);
+
+        $studentRole->givePermissionTo([
+            'view grades',
+            'view profile',
+            'view schedule',
+            'download materials',
+        ]);
+
+        $parentRole->givePermissionTo([
+            'view grades',
+            'view profile',
+        ]);
+    }
+}
